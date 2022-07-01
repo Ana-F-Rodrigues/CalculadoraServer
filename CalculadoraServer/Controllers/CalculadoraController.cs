bb1391fd9e14e9cad3fdcc8add63559c9ce58656
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-07-01 09:35:44 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraServer/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        //API está aberta escutando em uma porta 5000
+        [HttpGet("calcular")]
+        public IActionResult Calcular(string operador, double num1, double num2)
+        {
+            double result = 0;
+
+            switch (operador)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "/":
+                    if (num2 == 0)
+                        return Ok("NAO EXISTE!");
+                    else
+                        result = num1 / num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                default:
+                    return NotFound("Operador Não encontrado");
+            }
+
+            return Ok(result);
+        }
+    }
+ }
+
